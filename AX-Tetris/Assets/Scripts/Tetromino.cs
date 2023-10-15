617bf58fe4154a3538ba4281f4d94807c9f8896c
HEHEHEHEHEHEHEHEHEEEHEHEHEHEE <markhamonca@codeninjas.com>
2023-10-15 13:58:52 -0400

    65

--- AX-Tetris/Assets/Scripts/Tetromino.cs
+++ AX-Tetris/Assets/Scripts/Tetromino.cs
@@ -11,46 +11,40 @@ public class Tetromino : MonoBehaviour
     public static int width = 10;
     public static int height = 20;
     public static Transform[,] grid = new Transform[width, height];
-    // Update is called once per frame
-    public void AddToGrid()
-    {
-        foreach (Transform child in transform)
-        {
-            int x = Mathf.RoundToInt(child.transform.position.x);
-            int y = Mathf.RoundToInt(child.transform.position.y);
-            grid[x, y] = child;
-        }
 
-    }
+    // Update is called once per frame
     void Update()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.Translate(Vector2.up);
+            //transform.position += Vector3.up;
 
             Vector3 convertedPoint = transform.TransformPoint(rotationPoint);
             transform.RotateAround(convertedPoint, Vector3.forward, 90);
+            if (!ValidMove())
+            {
+                transform.RotateAround(convertedPoint, Vector3.forward, -90);
+            }
         }
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.Translate(Vector2.right);
+            transform.position += Vector3.right;
 
             if (!ValidMove())
             {
-                
-                transform.Translate(Vector2.left);
-
+                transform.position += Vector3.left;
             }
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector2.left);
-         
+            transform.position += Vector3.left;
+
             if (!ValidMove())
             {
-                transform.Translate(Vector2.right);
-                
+                transform.position += Vector3.right;
+
             }
 
         }
@@ -59,38 +53,57 @@ public class Tetromino : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow))
         {
             tempTime = tempTime / 10;
-           
+
 
         }
-
-
         if (Time.time - previousTime > tempTime)
         {
-            transform.Translate(Vector3.down);
+            transform.position += Vector3.down;
             previousTime = Time.time;
             if (!ValidMove())
             {
                 transform.position += Vector3.up;
                 this.enabled = false;
                 AddToGrid();
+                CheckLines();
                 FindObjectOfType<Spawner>().SpawnTetromino();
-               
+                
 
             }
         }
-                  
+        
     }
+
+    public void AddToGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            grid[x, y] = child;
+        }
+    }
+
     public bool ValidMove()
     {
         foreach (Transform child in transform)
         {
             int x = Mathf.RoundToInt(child.transform.position.x);
             int y = Mathf.RoundToInt(child.transform.position.y);
-                     
-            if (x < 0 || y < 0 || x >= width || y >= height)
+
+          
+
+
+            if (x < 0 || y < 0)
             {
                 return false;
             }
+
+            if (x >= width || y >= height)
+            {
+                return false;
+            }
+
             Debug.Log(x + " " + y);
             if (grid[x, y] != null)
             {
@@ -98,6 +111,58 @@ public class Tetromino : MonoBehaviour
             }
         }
         return true;
-        
+    }
+
+
+    public void CheckLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+
+
+    }
+
+    public bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public void DeleteLine(int i)
+    {
+      for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    public void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, y] != null)
+                {
+                    grid[x, y - 1] = grid[x, y];
+                    grid[x, y] = null;
+                    grid[x, y - 1].transform.position += Vector3.down;
+                }
+            }
+        }
     }
 }
