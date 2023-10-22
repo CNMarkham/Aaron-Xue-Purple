82458c466065a385a2b1c9eca4f48d39c48a42a1
Roosham <markhamonca@codeninjas.com>
2023-10-22 13:03:37 -0400

    prove yourself

--- AX-Find The Exit/Assets/ProveYourself/Scripts/PlayerMovement.cs
+++ AX-Find The Exit/Assets/ProveYourself/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+
+        }
+
+
         
     }
 }
