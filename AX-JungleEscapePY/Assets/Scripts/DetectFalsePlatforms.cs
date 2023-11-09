2cd2a8b485af80ffe7d8f92c59e61e22511ee216
Aroosh <markhamonca@codeninjas.com>
2023-11-09 17:49:51 -0500

    ninja run

--- AX-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
+++ AX-JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -7,6 +7,15 @@ public class DetectFalsePlatforms : MonoBehaviour
     public bool hit;
     void Update()
     {
-        
+        hit = Physics.Raycast(transform.position,transform.forward,2, 1 << 8);
+        if (hit == true)
+        {
+            Debug.LogWarning("Be careful!");
+        }
+        else
+        {
+            Debug.Log("All clear!");
+        }
     }
+    
 }
