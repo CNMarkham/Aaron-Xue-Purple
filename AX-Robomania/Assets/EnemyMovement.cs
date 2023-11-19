25304fc472ddc1a1db0c8a4e76b9974f42972b7e
Griff <markhamonca@codeninjas.com>
2023-11-19 14:02:13 -0500

    Finished Doctor Worm. Starting Robomania

---
+++ AX-Robomania/Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        float newXPosition = transform.position.x + speed * Time.deltaTime;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
