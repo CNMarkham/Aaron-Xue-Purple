7001a64679b67d474c2516fbcee61a9c1edb841f
Griff <markhamonca@codeninjas.com>
2023-12-03 13:59:10 -0500

    3f
    hi

--- AX-CyberFu/Assets/EnemyControls.cs
+++ AX-CyberFu/Assets/EnemyControls.cs
@@ -37,7 +37,7 @@ public class EnemyControls : MonoBehaviour
          
         if(currentAttackingTime > maxAttackingTime)
         {
-            EnemyAttack(Random.Range(1, 4));
+            EnemyAttack(Random.Range(1, 7));
             currentAttackingTime = 0f;
           
         }
@@ -60,6 +60,18 @@ public class EnemyControls : MonoBehaviour
         if (attack == 3)
         {
             animatorEnemy.SetTrigger("Attack3");
+        }
+        if (attack == 4)
+        {
+            animatorEnemy.SetTrigger("Attack4");
+        }
+        if (attack == 5)
+        {
+            animatorEnemy.SetTrigger("Attack5");
+        }
+        if (attack == 6)
+        {
+            animatorEnemy.SetTrigger("Attack6");
         }
     }
     void FollowTarget()
