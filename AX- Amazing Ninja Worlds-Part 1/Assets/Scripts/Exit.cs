16e82e4e36321690b849cfea3e5e88c81c2c236c
Hayden <markhamonca@codeninjas.com>
2024-01-14 13:39:45 -0500

    prove yourself

--- AX- Amazing Ninja Worlds-Part 1/Assets/Scripts/Exit.cs
+++ AX- Amazing Ninja Worlds-Part 1/Assets/Scripts/Exit.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
-    SpriteRenderer m_renderer;
     public GameObject gem;
-    public GameObject background;
-    public string teleportDestination;
-
-    private void Start()
-    {
-        m_renderer = gem.GetComponent<SpriteRenderer>();
-        
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if(m_renderer.enabled == false)
-        {
-            background.GetComponent<GameManager>().TeleportOpen(teleportDestination);
-        }
-
-    }
 }
